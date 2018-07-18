3d4ea15b4075c39faf6caf583d1f375e422b70dc
gmore <gmore@DESKTOP-LEKTV58>
2018-07-17 20:53:13 -0700

    Add project files.

---
+++ SimonSays/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace SimonSays
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.list1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.AutoEllipsis = true;
+            this.btn1.BackColor = System.Drawing.Color.Blue;
+            this.btn1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn1.Location = new System.Drawing.Point(77, 73);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(265, 111);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.Color.Red;
+            this.btn2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn2.Location = new System.Drawing.Point(401, 73);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(265, 111);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.Color.Gold;
+            this.btn4.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn4.Location = new System.Drawing.Point(401, 236);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(265, 111);
+            this.btn4.TabIndex = 2;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.Color.Green;
+            this.btn3.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn3.Location = new System.Drawing.Point(77, 236);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(265, 111);
+            this.btn3.TabIndex = 3;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(308, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 38);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // list1
+            // 
+            this.list1.GridLines = true;
+            this.list1.Location = new System.Drawing.Point(711, 86);
+            this.list1.Name = "list1";
+            this.list1.Size = new System.Drawing.Size(91, 252);
+            this.list1.TabIndex = 5;
+            this.list1.UseCompatibleStateImageBehavior = false;
+            this.list1.View = System.Windows.Forms.View.Details;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(216, 394);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Time remaining";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(310, 395);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(590, 394);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Try #";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(628, 394);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(814, 458);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.list1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListView list1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ SimonSays/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace SimonSays
+{
+    public partial class Form1 : Form
+    {
+        Queue<int> order = new Queue<int>();
+        Queue<int> userOrder = new Queue<int>();
+        int tries = 1, level = 1; 
+
+        public Form1()
+        {
+            InitializeComponent();
+            list1.Columns.Add("Queue", 100);
+        }
+
+        public async void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            foreach (int number in order)
+            {
+                switch (number)
+                {
+                    case 1:
+                            btn1.BackColor = Color.Purple;
+                            await Task.Delay(1000);
+                            btn1.BackColor = Color.Blue;
+                        break;
+                    case 2:
+                            btn2.BackColor = Color.Purple;
+                            await Task.Delay(1000);
+                            btn2.BackColor = Color.Red;
+                        break;
+                    case 3:
+                            btn3.BackColor = Color.Purple;
+                            await Task.Delay(1000);
+                            btn3.BackColor = Color.Green;
+                        break;
+                    case 4:
+                        btn4.BackColor = Color.Purple;
+                        await Task.Delay(1000);
+                        btn4.BackColor = Color.Gold;
+                        break;
+                }
+                await Task.Delay(1000);
+            }
+            playersTurn(); 
+        }
+        public async void playersTurn()
+        {
+            try
+            {
+                MessageBox.Show("It's your turn to play!");
+                int count = 0;
+                for (int i = 5; i >= 0; i--)
+                {
+                    label2.Text = i.ToString();
+                    await Task.Delay(1000);
+                }
+                for (int i = 0; i < tries; i++)
+                {
+                    if (order.ElementAt(i) != userOrder.ElementAt(i))
+                    {
+                        count++;
+                        MessageBox.Show("Game Over");
+                        i = tries +1 ;
+                        tries = 1; 
+                    }
+                }
+                if (count == 0)
+                {
+                    MessageBox.Show("Congratulations!", "Yeah!");
+                    tries++;
+                    generateRandomNumber();
+                }
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Game Over");
+            }
+        }
+        public void generateRandomNumber()
+        {
+            list1.Clear();
+            list1.Columns.Add("Queue", 100);
+            userOrder.Clear();
+            label4.Text = tries.ToString();
+            Random newNumber = new Random();
+            for (int i = 0; i < level; i++)
+            {
+                order.Enqueue(newNumber.Next(1, 5));
+            }
+            lightRectangles(order);
+        }
+        public void lightRectangles(Queue<int> order)
+        {
+            timer1.Start(); 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            order.Clear();
+            userOrder.Clear();
+            list1.Clear();
+            list1.Columns.Add("Queue", 100);
+            label2.Text = "";
+            label4.Text = tries.ToString();
+            generateRandomNumber();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            userOrder.Enqueue(1);
+            list1.Items.Add(1.ToString());
+            list1.Show();
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            userOrder.Enqueue(2);
+            list1.Items.Add(2.ToString());
+            list1.Show();
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            userOrder.Enqueue(3);
+            list1.Items.Add(3.ToString());
+            list1.Show();
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            userOrder.Enqueue(4);
+            list1.Items.Add(4.ToString());
+            list1.Show();
+        }
+    }
+}
